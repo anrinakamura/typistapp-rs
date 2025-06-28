3669dd727eba9c5d514a0c1d59057c535e6d7d28
anrinakamura <89412831+anrinakamura@users.noreply.github.com>
2025-06-29 04:30:47 +0900

    feat: load font and calculate luminance from character.  (#3)
    * update
    
    * update
    
    * update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use anyhow::Result;
+use typistapp::model::Model;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[allow(dead_code)]
+fn run() -> Result<()> {
+    let font_data = std::fs::read("path/to/font.ttf")?;
+    let _ = Model::from_vec(font_data)?;
+    Ok(())
 }
