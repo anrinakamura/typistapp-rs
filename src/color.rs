d8bf630ff5324633314aa46801274cf470107a22
anrinakamura <89412831+anrinakamura@users.noreply.github.com>
2025-07-09 02:23:18 +0900

    fix

--- src/color.rs
+++ src/color.rs
@@ -15,8 +15,8 @@ impl Color {
     /// # Example
     ///
     /// ```no_run
-    /// use typistapp::color::Color; 
-    /// 
+    /// use typistapp::color::Color;
+    ///
     /// let luminance = Color::luminance_from_rgba(&[255, 255, 255, 255]);
     /// assert!(luminance > 0.9);
     /// ```
