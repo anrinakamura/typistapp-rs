03b17adc73773bd4002c7410cad10d9e18b1c395
anrinakamura <89412831+anrinakamura@users.noreply.github.com>
2025-07-19 15:48:11 +0900

    fix

--- src/element.rs
+++ src/element.rs
@@ -188,15 +188,13 @@ impl Element {
 
 #[cfg(test)]
 mod tests {
+    use crate::FONT_DATA;
     use super::*;
-    use std::fs;
-
-    const FONT_PATH: &str = "resources/NotoSansJP-Regular.otf";
 
     #[test]
     fn element_from_char() {
-        let font_data = fs::read(FONT_PATH).unwrap();
-        let font = FontArc::try_from_vec(font_data).unwrap();
+        // let font_data = fs::read().unwrap();
+        let font = FontArc::try_from_slice(FONT_DATA).unwrap();
         let scale = PxScale::from(16.0);
         let element = Element::from_char(&font, 'A', scale);
         assert!(element.is_ok());
